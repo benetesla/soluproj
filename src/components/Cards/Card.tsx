8893b93258a68e479686a8ebac0c81a93d31e4bb
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-06-10 17:45:23 -0300

    :sparkles: criando os cartoes da aplicacao

---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,37 @@
+
+import './Card.scss'
+const Card = () => {
+    return (
+        <div className='card'>
+            <div >
+                <ul className='card__list'>
+                    <li className='card__item'>HTML</li>
+                    <li className='card__item'>CSS</li>
+                    <li className='card__item'>JS</li>
+                    <li className='card__item'>React</li>
+                    <li className='card__item'>Node</li>
+                    <li className='card__item'>Express</li>
+                    <li className='card__item'>MongoDB</li>
+                    <li className='card__item'>Git</li>
+                    <li className='card__item'>GitHub</li>
+                    <li className='card__item'>TypeScript</li>
+                    <li className='card__item'>SASS</li>
+                    <li className='card__item'>Bootstrap</li>
+                    <li className='card__item'>Material UI</li>
+                    <li className='card__item'>React Native</li>
+                    <li className='card__item'>Redux</li>
+                    <li className='card__item'>Next</li>
+                    <li className='card__item'>Gatsby</li>
+                    <li className='card__item'>GraphQL</li>
+                    <li className='card__item'>Apollo</li>
+                    <li className='card__item'>Firebase</li>
+                    <li className='card__item'>Heroku</li>
+                    <li className='card__item'>Netlify</li>
+                    <li className='card__item'>Vercel</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Card
